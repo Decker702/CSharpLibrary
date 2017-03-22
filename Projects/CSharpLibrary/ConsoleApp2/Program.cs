a6002de215c3738489da8b2312be928577276d95
LDecker <Decker702@yahoo.com>
2017-03-22 13:41:26 -0400

    Strings and Conditionals

---
+++ Projects/CSharpLibrary/ConsoleApp2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  BRONZE CHALLENGE:
+                Create a list of value types. (int, string, bool, float, double, decimal)
+                Create two examples for each type. One is a declaration only.
+                The other is a declaration with initializer.
+                Print a view of the values in the console.
+
+            SILVER CHALLENGE:
+                Use those types to concatenate a string.Any string will do.
+
+
+           GOLD CHALLENGE:
+              Declare the year you were born as a string.
+                Convert that variable from a string to an integer and print it with a message in the
+                  console.
+                For instance, "I was born in 1976." */
+
+            //Bronze
+            int luckyNumber;
+            int number = 345;
+
+            string Post;
+            string name = "Paul";
+
+            bool isMarried;
+            bool answer = true;
+
+            float amountOfSnow;
+            float temperature = 234.4f;
+
+            double lattitude;
+            double oneThird = 3.333;
+
+            decimal StudentLoanAmount;
+            decimal around = 234.45; //more expensive than a doiuble
+
+            Console.WriteLine(temperature);
+            Console.WriteLine(oneThird);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Projects/CSharpLibrary/ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Simple Demo
+            string firstName;
+            string lastName;
+            string occupation;
+
+            firstName = "Charlie";
+            lastName = "Brown";
+            occupation = "Stuntman";
+
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+
+            //String Interpolation
+            Console.WriteLine("Name: {0} \n Occupation: {1}\n {2}",
+            fullName,
+            occupation,
+            "Paul"
+            );
+            //String Interpolation
+            Console.WriteLine("{0:C}",100.736);
+
+
+
+            /*  Bronze Challenge:
+             *  Try to show how to concatenate multiple strings, 
+             *  use composite formating, 
+             *  show how to use escapes, 
+             *  and how to manipulate numbers(eg, dollars &commas).*/
+
+           
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
