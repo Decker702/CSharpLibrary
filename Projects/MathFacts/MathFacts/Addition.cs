b56719c72d6e550aa2dc68619c889168ab9fb6ad
LDecker <Decker702@yahoo.com>
2017-03-29 15:33:11 -0400

    Work on Math App

---
+++ Projects/MathFacts/MathFacts/Addition.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFacts
+{
+    class Addition
+    {
+        public void AdditionTitle()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            string appTitle = @"
+   __                            ______      _  
+  /  )   /   /  _/_                /     /  //  
+ /--/ __/ __/ o /  o __ ____    --/__.  /__// _ 
+/  (_(_/_(_/_<_<__<_(_)/ / <_  (_/(_/|_/_)</_</_
+                                                ";
+            Console.WriteLine(appTitle);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("You can select a starting number and an ending number between 1 - 10.");
+            Console.WriteLine("Then you will see the numbers between your selections be added");
+            Console.WriteLine("to numbers 1 - 10");
+            Console.WriteLine("");
+            Console.WriteLine("Select a starting number between 1 and 10");
+        }
+
+        public void AdditionTable(int startNum, int endNum)
+        {
+            for (int i = startNum - 1; i <= endNum; i++)
+            {
+                if (i == startNum - 1)
+                {
+                    Console.Write(String.Format("{0, 6}", "n"));
+                }
+                else
+                {
+                    Console.Write(String.Format("{0,6}", i));
+                }
+
+            }
+
+            Console.WriteLine("\n");
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(String.Format("{0, 6}", i));
+                for (int b = startNum; b <= endNum; b++)
+                {
+                    string output = String.Format("{0, 6}", i + b);
+                    Console.Write(output);
+                }
+                Console.WriteLine("");
+            }
+
+
+        }
+    }
+}
+
+/*
+public void AdditionTable()
+
+{
+    Console.Clear(); //has to be inside a me .d888888        dP       dP oo   dP   oo                   
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    string appTitle = @" .d888888        dP       dP oo   dP   oo                   
+d8'    88        88       88      88                        
+88aaaaa88a .d888b88 .d888b88 dP d8888P dP .d8888b. 88d888b. 
+88     88  88'  `88 88'  `88 88   88   88 88'  `88 88'  `88 
+88     88  88.  .88 88.  .88 88   88   88 88.  .88 88    88 
+88     88  `88888P8 `88888P8 dP   dP   dP `88888P' dP    dP 
+
+
+d888888P          dP       dP          
+88             88       88          
+88    .d8888b. 88d888b. 88 .d8888b. 
+88    88'  `88 88'  `88 88 88ooood8 
+88    88.  .88 88.  .88 88 88.  ... 
+dP    `88888P8 88Y8888' dP `88888P' ";
+    Console.WriteLine(appTitle);
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("The starting and ending numbers are between 1 and 10.");
+    Console.WriteLine("Next you will see the numbers between these numbers added");
+    Console.WriteLine("");
+    Console.WriteLine("Select a starting number between 1 and 10"); 
+   // Console.ReadLine();
+}
+public void AdditionTable(int startNum, int endNum)
+{
+    //TODO - build out the logic of the addition table here.
+    for (int i = startNum - 1; i <= endNum; i++)
+    {
+        //using Write to get them to be side by side.  
+        Console.Write(String.Format("{ 0, 6}", n));
+    }
+    else
+    {
+        Console.Write(String.Format("{0, 6}", i));
+    }
+    Console.WriteLine("");
+
+    for (int i = 1; i < 10; i++)
+    {
+        Console.Write(String.Format("{0, 6}"));
+        for (int b = startNum; b <= endNum; b++);
+
+        {
+            string output = String.Format("{0, 6}");
+            Console.Write(output);
+        }
+        Console.WriteLine("");
+    }
+}
+}
+}
+*/
---
+++ Projects/MathFacts/MathFacts/Program.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //with static you don't need an instance to use a method
+
+        {
+            Console.Title = "Math Facts App";
+            int choice = 0;
+            Addition addTable = new Addition();
+            Multiplication timesTable = new Multiplication();
+            do
+            {
+                MainAppTitle();
+                choice = MainMenu();
+                if (choice == 1)
+                {
+                    bool continueChoice = true;
+                    int startNum = 0;
+                    int endNum = 0;
+                    do
+                    {
+                        addTable.AdditionTitle();
+                        try
+                        {
+                            Console.WriteLine("Enter your starting number");
+                            startNum = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter your ending number");
+                            endNum = Int32.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Please enter a valid number");
+                            Console.ReadLine();
+                        }
+                        finally
+                        {
+                            if (startNum > 0 && endNum <= 10)
+                            {
+                                addTable.AdditionTitle();
+                                Console.WriteLine("");
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                Console.WriteLine("Your addition Table for {0} - {1}", startNum, endNum);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.WriteLine("");
+                                addTable.AdditionTable(startNum, endNum);
+                                Console.WriteLine("");
+                                try
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine("Would you like more additon facts [y/n]");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    string moreFacts = Console.ReadLine();
+                                    if (moreFacts == "y")
+                                    {
+                                        continueChoice = true;
+                                    }
+                                    else if (moreFacts == "n")
+                                    {
+                                        continueChoice = false;
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("");
+                                    Console.WriteLine("Enter y or n");
+                                }
+                            }
+                        }
+
+                    } while (continueChoice == true);
+                }
+
+                //Multiplication portion
+                if (choice == 2)
+                {
+                    bool continueChoice = true;
+                    int startNum = 0;
+                    int endNum = 0;
+                    do
+                    {
+                        timesTable.MultiplicationTitle();
+                        try
+                        {
+                            Console.WriteLine("Enter your starting number");
+                            startNum = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter your ending number");
+                            endNum = Int32.Parse(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Please enter a valid number");
+                            Console.ReadLine();
+                        }
+                        finally
+                        {
+                            if (startNum > 0 & endNum <= 10)
+                            {
+                                timesTable.MultiplicationTitle();
+                                Console.WriteLine("");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("Your Multiplication Table for {0} - {1}", startNum, endNum);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.WriteLine("");
+                                timesTable.MultiplicationTable(startNum, endNum);
+                                Console.WriteLine("");
+
+                                try
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine("Would you like more multiplication facts [y/n]");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    string moreFacts = Console.ReadLine();
+                                    if (moreFacts == "y")
+                                    {
+                                        continueChoice = true;
+                                    }
+                                    else if (moreFacts == "n")
+                                    {
+                                        continueChoice = false;
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("");
+                                    Console.WriteLine("Enter y or n");
+                                }
+                            }
+                        }
+                    } while (continueChoice == true);
+                }
+            } while (choice != 3);
+        }
+
+        private static int MainMenu()
+        {
+            int choice;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Select an option");
+            Console.WriteLine("-----------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Option 1: Addition Facts");
+            Console.WriteLine("Option 2: Multiplication Facts");
+            Console.WriteLine("Option 3: Leave Math Facts");
+            //TODO - fix the exception handling
+            choice = Int32.Parse(Console.ReadLine());
+            return choice;
+        }
+
+        private static void MainAppTitle()
+        {
+            Console.Clear();
+            string appTitle = @"
+
+ _ _ _                 _____                 __        
+' ) ) )     _/_ /       /  '      _/_       /  )       
+ / / / __.  /  /_    ,-/-,__.  _. /  _     /--/ _   _  
+/ ' (_(_/|_<__/ /_  (_/  (_/|_(__<__/_)_  /  (_/_)_/_)_
+                                              /   /    
+                                             '   '    ";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(appTitle);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
+/*
+static void Main(string[] args)
+
+{
+    Console.Clear(); //clears screen between selections
+    //Change the state of the applications.
+    // Initial state - menu, then 3 items are other states(choices)
+    //First - Build a title
+    Console.Title = "Math Facts App";
+    int choice = 0;
+    Addition addTable = new Addition();
+    //Do While Loop
+    do
+    {
+        NewMethod(); //this is the ascii art file
+        choice = NewMethod1();// this is the main menu
+        if (choice == 1)
+        {
+            bool continueChoice = true;
+            int startNum = 0;
+            int endNum = 0;
+            do
+            {
+                addTable.AdditionTable();
+                try
+                {
+                    Console.WriteLine("Enter your stating number.");
+                    startNum = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter your ending number.");
+                    endNum = Int32.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    Console.ReadLine();
+                    break;
+                }
+                finally
+                {
+                    if (startNum > 0 && endNum <= 10)
+                    {
+                        addTable.AdditionTable();
+                        Console.WriteLine("");//to add a space
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Your Addition Table for {0} to {1}", startNum, endNum);
+                        Console.WriteLine("");
+                        addTable.AdditionTable(startNum, endNum);
+
+                        Console.WriteLine("");
+                        try
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("Would you like more addion facts? [y/n]");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            string moreFacts = Console.ReadLine();
+                            if (moreFacts == "y")
+                            {
+                                continueChoice = false;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                    //Build logic to handle addition table
+                    //build logic to allow user to continue or end addition
+                    //Console.WriteLine("Hey you did it");
+                    //Console.ReadLine();
+                    //continueChoice == false;
+
+                }
+
+            } while (continueChoice == true);
+        }
+        if (choice == 2)
+        {
+            bool continueChoice = true;
+            int startNum = 0;
+            int endNum = 0;
+            do
+            {
+
+            }while ()
+        }
+
+            //Console.ReadLine();
+            //break;
+
+            //Learned to refractered by extracting it to a helper method
+        } while (choice != 3) ;//this is available to everthing in here
+                               //above lets menu continue as long as don't enter 3.
+//When choice equals 3, then it is false & ends.
+
+
+    }
+private static int NewMethod1()
+{
+    int choice;
+    Console.WriteLine("Select an option");
+    Console.WriteLine("-----------------");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Option 1: Addition Facts");
+    Console.WriteLine("Option 2: Multiplication Facts");
+    Console.WriteLine("Option 3: Leave Math Facts");
+    choice = Int32.Parse(Console.ReadLine());
+    return choice;
+}
+
+private static void NewMethod()
+{
+    string appTitle = @"
+8888ba.88ba             dP   dP           88888888b                     dP            
+88  `8b  `8b            88   88           88                            88            
+88   88   88 .d8888b. d8888P 88d888b.    a88aaaa    .d8888b. .d8888b. d8888P .d8888b. 
+88   88   88 88'  `88   88   88'  `88     88        88'  `88 88'  `""   88   Y8ooooo. 
+88   88   88 88.  .88   88   88    88     88        88.  .88 88.  ...   88         88 
+dP   dP   dP `88888P8   dP   dP    dP     dP        `88888P8 `88888P'   dP   `88888P' 
+
+
+.d888888                    
+d8'    88                    
+88aaaaa88a 88d888b. 88d888b. 
+88     88  88'  `88 88'  `88 
+88     88  88.  .88 88.  .88 
+88     88  88Y888P' 88Y888P' 
+   88       88       
+   dP       dP       ";
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(appTitle);
+
+}
+}
